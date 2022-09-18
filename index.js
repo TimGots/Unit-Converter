00bb1b3752b7dece29e2ce4bf0dc02059b556020
TimGots <111915234+TimGots@users.noreply.github.com>
2022-09-18 16:57:52 +0300

    final version

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const meterToFeet =  3.281
-const literToGallon =  0.264
-const kiloToPound =  2.204
+const meterToFeet = 3.281
+const literToGallon = 0.264
+const kiloToPound = 2.204
 
 let btnEl = document.getElementById("btn-el")
 let inputEl = document.getElementById("input-el")
-let lengthEl = document.getElementById("length")
-btnEl.addEventListener("click",function(){
-    let baseValue = inputEl.value
-    lengthEl.textContent = `${baseValue} meters = ${(baseValue*meterToFeet).toFixed(2)} feet`
+const lengthEl = document.getElementById("length")
+const volumeEl = document.getElementById("volume")
+const massEl = document.getElementById("mass")
 
-    
+btnEl.addEventListener("click", function () {
+    let baseValue = inputEl.value
+    lengthEl.textContent = `${baseValue} meters = ${(baseValue * meterToFeet).toFixed(2)} feet`
+    volumeEl.textContent = `${baseValue} liters = ${(baseValue * literToGallon).toFixed(2)} gallons`
+    massEl.textContent = `${baseValue} kilograms = ${(baseValue * kiloToPound).toFixed(2)} pounds`
     
 })
