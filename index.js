59b83768edd8a8336b50e13f9fbc053f8f8786ec
TimGots <111915234+TimGots@users.noreply.github.com>
2022-09-19 10:24:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const massEl = document.getElementById("mass")
 
 btnEl.addEventListener("click", function () {
     let baseValue = inputEl.value
-    lengthEl.textContent = `${baseValue} meters = ${(baseValue * meterToFeet).toFixed(2)} feet`
-    volumeEl.textContent = `${baseValue} liters = ${(baseValue * literToGallon).toFixed(2)} gallons`
-    massEl.textContent = `${baseValue} kilograms = ${(baseValue * kiloToPound).toFixed(2)} pounds`
+    lengthEl.textContent = `${baseValue} meters = ${(baseValue * meterToFeet).toFixed(2)} feet | ${baseValue} feets = ${(baseValue/meterToFeet).toFixed(2)} meters`
+    volumeEl.textContent = `${baseValue} liters = ${(baseValue * literToGallon).toFixed(2)} gallons | ${baseValue} gallons = ${(baseValue/literToGallon).toFixed(2)} liters`
+    massEl.textContent = `${baseValue} kilograms = ${(baseValue * kiloToPound).toFixed(2)} pounds | ${baseValue} pounds = ${(baseValue/kiloToPound).toFixed(2)} kilograms`
     
 })
